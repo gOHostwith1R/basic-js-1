c89aa01372d2367a0c96f1dc8a29b19083369e53
RoslikIgor <roslik2014@gmail.com>
2020-09-27 16:12:59 +0300

    feat: dream-team done

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,25 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+//const CustomError = require("../extensions/custom-error");
+//module.exports = 
+module.exports =  function createDreamTeam(members) {
+  if(!Array.isArray(members)) {
+    return false;
+  }
+  let str='';
+  members.forEach(element => {  
+    if(typeof element != 'string') {
+      return false
+    } else {
+      element = element.trim()
+      str+=element[0]
+    }
+  });
+  console.log(str);
+  str = str.toUpperCase()
+  .split('')
+  .sort(function(a,b) {
+    return a.charCodeAt() - b.charCodeAt();
+  })
+  .join('')
+  console.log(str);
+  return str;
 };
