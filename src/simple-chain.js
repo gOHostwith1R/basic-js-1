e77fca3a0e3940dbc8b3732fdc78866e9cdf12e0
RoslikIgor <roslik2014@gmail.com>
2020-09-28 13:27:30 +0300

    feat: simple-chain done

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  makeChain: [],
+  counter: 0,
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.counter;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.counter++;
+    (value === undefined) ? this.makeChain.push('( )'): this.makeChain.push(`( ${value} )`);
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if(!(Number.isInteger(position)) || position>this.counter || position === undefined) {
+      this.makeChain = []
+      throw new Error();
+    } else {
+      this.makeChain.splice(position-1,1);
+    }
+    return this
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.makeChain = this.makeChain.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+   let res = this.makeChain.join('~~');
+   this.makeChain = [];
+   return res;
+  } 
 };
 
 module.exports = chainMaker;
+
+//console.log(chainMaker.addLink('GHI').addLink(null).reverseChain().addLink(333).reverseChain().reverseChain().addLink(0).reverseChain().reverseChain().addLink('GHI').finishChain());
